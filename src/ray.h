d39881168302044b2db15be2ce4a25da934d713d
Zachary Kirtz <zach.kirtz@gmail.com>
2026-02-02 15:30:37 -0600

    ray: Implement initial ray class

---
+++ src/ray.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "vec.h"
+
+class ray {
+public:
+    constexpr ray() : m_origin{}, m_direction{} {}
+
+    constexpr ray(const Vector3DF& orig, const Vector3DF& dir) : m_origin(orig), m_direction(dir) {}
+
+    constexpr const Vector3DF& origin() const { return m_origin; }
+    constexpr const Vector3DF& direction() const { return m_direction; }
+
+    constexpr Vector3DF at(float t) const {
+        return m_origin + m_direction * t;
+    }
+
+private:
+    Vector3DF m_origin;
+    Vector3DF m_direction;
+}; // class ray
