96bda7ee53e26966fb030ae7596c0f9fcd727d8e
Zachary Kirtz <zach.kirtz@gmail.com>
2026-02-04 22:33:28 -0600

    PerspectiveCamera: Fix some issues, it works now :)

--- src/PerspectiveCamera.cpp
+++ src/PerspectiveCamera.cpp
@@ -1,15 +1,18 @@
 #include "PerspectiveCamera.h"
+#include <print>
 
 namespace eng {
 
 ray PerspectiveCamera::GenerateRay(int i, int j) {
-    const auto right_bound  = static_cast<float>(m_img_plane_width) / 2;
+    const auto right_bound  = static_cast<float>(m_img_plane_width) / 2.0;
     const auto left_bound   = -right_bound;
-    const auto top_bound    = static_cast<float>(m_img_plane_width) / 2;
+    const auto top_bound    = static_cast<float>(m_img_plane_width) / 2.0;
     const auto bottom_bound = -top_bound;
 
-    const float u = left_bound + (right_bound - left_bound) * (static_cast<float>(i) + 0.5);
-    const float v = bottom_bound + (top_bound - bottom_bound) * (static_cast<float>(j) + 0.5);
+    std::print(stderr, "{} {} {} {} {} {}\n", right_bound, left_bound, top_bound, bottom_bound, m_img_plane_width, m_img_plane_height);
+
+    const float u = left_bound + (right_bound - left_bound) * (static_cast<float>(i) + 0.5) / static_cast<float>(m_img_width);
+    const float v = bottom_bound + (top_bound - bottom_bound) * (static_cast<float>(j) + 0.5) / static_cast<float>(m_img_height);
 
     const Vector3DF direction = -m_focal_length * m_W + u * m_U + v * m_V;
     const Vector3DF origin    = m_position;
--- src/PerspectiveCamera.h
+++ src/PerspectiveCamera.h
@@ -10,12 +10,35 @@ public:
         m_position(), m_U(1,0,0), m_V(0,1,0), m_W(0,0,1),
         m_focal_length(1.0), m_img_plane_width(2), m_img_plane_height(2) {}
 
+    constexpr PerspectiveCamera(const Vector3DF& pos, const Vector3DF& dir, float focal_length, int img_width, int img_height, float img_plane_height) :
+        m_position(pos), m_U(), m_V(), m_W(), m_img_width(img_width), m_img_height(img_height), m_focal_length(focal_length), m_img_plane_width(0), m_img_plane_height(img_plane_height)
+    {
+        /* Calculate plane width. */
+        const auto aspect_ratio = static_cast<float>(m_img_width) / static_cast<float>(m_img_height);
+        m_img_plane_width = m_img_plane_height * aspect_ratio;
+
+        /* W is just the direction normalized, since W _is_ the direction we're facing. */
+        m_W = dir.normalize();
+
+        /* V points to our right. We'll use a cross product with "up" to get right. We don't yet have an up, so use absolute up for now. */
+        {
+            static constexpr Vector3DF b {0, 1, 0};
+            m_U = cross(m_W, b).normalize();
+        }
+
+        /* U points up, take a cross product with our forward and right to get up. */
+        m_V = cross(m_U, m_W).normalize();
+    }
+
+    virtual ~PerspectiveCamera() = default;
+
     virtual ray GenerateRay(int i, int j) override;
 private:
     Vector3DF m_position;
     Vector3DF m_U, m_V, m_W;
+    int m_img_width, m_img_height;
     float m_focal_length;
-    int m_img_plane_width, m_img_plane_height;
+    float m_img_plane_width, m_img_plane_height;
 }; // class PerspectiveCamera
 
 } // namespace eng
