53931941757a5746516863b04b0f455c342d9a6a
Zachary Kirtz <zach.kirtz@gmail.com>
2026-02-06 21:17:12 -0600

    main: Use NX display framebuffer exporter on NX plat (works :))

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,15 @@
 #include "PerspectiveCamera.h"
-#include "Framebuffer.h"
+#include "framebuffer/Framebuffer.h"
+#include "framebuffer/PngWriter.h"
 
-int main() {
+#include <print>
+#include <cstdio>
+
+#ifdef __SWITCH__
+#include "framebuffer/nx/NativeWindowWriter.plat-nx.h"
+#endif
+
+extern "C" int main(int argc, char** argv) {
     int img_width  = 1280;
     int img_height = 720;
 
@@ -11,8 +19,8 @@ int main() {
     float vp_width  = 0.5;
 
     eng::Vector3DF pos{3, -4, 12};
-    eng::Vector3DF dir{-8, -5, 10};
-    float foc_len = 0.001;
+    eng::Vector3DF dir{69, 420, 64};
+    float foc_len = 0.01;
     
     eng::PerspectiveCamera cam(pos, dir, foc_len, img_width, img_height, vp_width);
     eng::Framebuffer fb(img_width, img_height);
@@ -27,5 +35,45 @@ int main() {
         }
     }
 
-    fb.ExportToPng("test.png");
+#ifndef __SWITCH__
+    eng::PngWriter png_writer("test.png");
+    png_writer.WriteFramebuffer(fb);
+#else
+    // Configure our supported input layout: a single player with standard controller styles
+    padConfigureInput(1, HidNpadStyleSet_NpadStandard);
+
+    // Initialize the default gamepad (which reads handheld mode inputs as well as the first connected controller)
+    PadState pad;
+    padInitializeDefault(&pad);
+
+
+    ::NWindow* nwin = ::nwindowGetDefault();
+
+    /* Create our framebuffer. */
+    ::Framebuffer native_fb;
+    ::framebufferCreate(&native_fb, nwin, 1280, 720, PIXEL_FORMAT_RGBA_8888, 2);
+    ::framebufferMakeLinear(&native_fb);
+
+    /* Create NativeWindow fb writer. */
+    eng::plat::nx::NativeWindowWriter writer(&native_fb);
+    writer.WriteFramebuffer(fb);
+
+    // Main loop
+    while (appletMainLoop()) {
+        // Scan the gamepad. This should be done once for each frame
+        padUpdate(&pad);
+
+        // padGetButtonsDown returns the set of buttons that have been
+        // newly pressed in this frame compared to the previous one
+        u64 kDown = padGetButtonsDown(&pad);
+
+        if (kDown & HidNpadButton_Plus)
+            break; // break in order to return to hbmenu
+
+    }
+
+    /* Finalize the native framebuffer. */
+    ::framebufferClose(&native_fb);
+
+#endif
 }
