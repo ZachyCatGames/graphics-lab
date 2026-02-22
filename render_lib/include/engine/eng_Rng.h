aa3fa82be637052faa7bef75a0afa2e4e182c720
Zachary Kirtz <zach.kirtz@gmail.com>
2026-02-22 15:57:45 -0600

    render_lib: Rng: Add basic rng class.

---
+++ render_lib/include/engine/eng_Rng.h
@@ -0,0 +1,43 @@
+#pragma once
+#include <ctime>
+#include <type_traits>
+#include <random>
+
+namespace eng {
+
+template<typename T>
+requires std::is_arithmetic_v<T>
+class Rng {
+public:
+    using value_type = std::remove_reference_t<T>;
+
+    Rng(value_type min, value_type max) : m_dist(min, max) {}
+
+    void Reset() { m_dist.reset(); }
+
+    auto GetNext() {
+        if (!s_init)
+            s_rng.seed(std::time(nullptr));
+        return m_dist(s_rng);
+    }
+
+    auto operator()() { return this->GetNext(); }
+private:
+    static std::mt19937 s_rng;
+    static constinit bool s_init;
+private:
+    //static_assert(std::is_integral_v<value_type>);
+    //using DistType = std::uniform_real_distribution<float>;
+    using DistType = std::conditional_t<std::is_integral_v<value_type>, std::uniform_int_distribution<value_type>, std::uniform_real_distribution<value_type>>;
+    DistType m_dist;
+}; // class Rng
+
+template<typename T>
+requires std::is_arithmetic_v<T>
+std::mt19937 Rng<T>::s_rng;
+
+template<typename T>
+requires std::is_arithmetic_v<T>
+constinit bool Rng<T>::s_init = false;
+
+} // namespace eng
