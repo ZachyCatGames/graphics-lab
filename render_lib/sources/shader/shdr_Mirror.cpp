81805ef7429b66fe3c2390e423bd5fb1a07cbab1
Zachary Kirtz <zach.kirtz@gmail.com>
2026-02-25 12:43:56 -0600

    render_lib: shdr: Push current progress on Mirror shader (no worky)

---
+++ render_lib/sources/shader/shdr_Mirror.cpp
@@ -0,0 +1,17 @@
+#include <engine/shader/shdr_Mirror.h>
+#include <engine/eng_Scene.h>
+
+namespace eng::shdr {
+
+Vector3DF Mirror::GetColor(Scene* p_scene, const HitStruct& rec) {
+    const auto dir = rec.r.direction().normalize();
+    const auto nor = rec.normal.normalize();
+    const Ray r {
+        rec.position,
+        (dir - 2 * dot(dir, nor) * nor).normalize(),
+    };
+
+    return p_scene->GetRayColor(r);
+}
+
+} // namespace eng
