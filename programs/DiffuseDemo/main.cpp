ac2f6cb0abb1f49af023bb8b33011ce8482aaca2
Zachary Kirtz <zach.kirtz@gmail.com>
2026-03-01 20:35:09 -0600

    programs: Diffuse/Lambertain demos: make ball purple + add an RGB triforce :)

--- programs/DiffuseDemo/main.cpp
+++ programs/DiffuseDemo/main.cpp
@@ -29,14 +29,40 @@ int main(int argc, char** argv) {
         shdr::FlatColor::Create(Vector3DF(0.75, 0.75, 0.75))
     ));
 
+    /* Create color shaders. */
+    auto red_shader = shdr::Diffuse::Create(
+        shdr::FlatColor::Create(Vector3DF(1, 0, 0))
+    );
+    auto green_shader = shdr::Diffuse::Create(
+        shdr::FlatColor::Create(Vector3DF(0, 1, 0))
+    );
+    auto blue_shader = shdr::Diffuse::Create(
+        shdr::FlatColor::Create(Vector3DF(0, 0, 1))
+    );
+
+    static constexpr float TriBaseX = -7.0;
+    static constexpr float TriBaseY = 22.0;
+    static constexpr float TriBaseZ = -30;
+
+    /* Create The Triforce. */
+    p_scene->EmplaceShape<shape::Triangle>(
+        Vector3DF(TriBaseX-10, TriBaseY, TriBaseZ), Vector3DF(TriBaseX, TriBaseY, TriBaseZ), Vector3DF(TriBaseX-5, TriBaseY+10, TriBaseZ)
+    ).BindShader(blue_shader);
+    p_scene->EmplaceShape<shape::Triangle>(
+        Vector3DF(TriBaseX, TriBaseY, TriBaseZ), Vector3DF(TriBaseX+10, TriBaseY, TriBaseZ), Vector3DF(TriBaseX+5, TriBaseY+10, TriBaseZ)
+    ).BindShader(green_shader);
+    p_scene->EmplaceShape<shape::Triangle>(
+        Vector3DF(TriBaseX-5, TriBaseY+10, TriBaseZ), Vector3DF(TriBaseX+5, TriBaseY+10, TriBaseZ), Vector3DF(TriBaseX, TriBaseY+20, TriBaseZ)
+    ).BindShader(red_shader);
+
     /* Create a light emitter. */
     p_scene->EmplaceShape<shape::Sphere>(Vector3DF(0, 30, -10), 5)
         .BindShader(shdr::Emitter::Create(Vector3DF(30, 30, 30)));
 
     /* Make a sphere. */
-    p_scene->EmplaceShape<eng::shape::Sphere>(eng::Vector3DF{3, 6, -10}, 5)
+    p_scene->EmplaceShape<eng::shape::Sphere>(eng::Vector3DF{4, 6, -10}, 5)
         .BindShader(shdr::Diffuse::Create(
-            shdr::FlatColor::Create(Vector3DF(0.0, 0.75, 0.0))
+            shdr::FlatColor::Create(Vector3DF(1,0,1))
     ));
 
     /* And another one. */
--- programs/LambertianDemo/main.cpp
+++ programs/LambertianDemo/main.cpp
@@ -36,34 +36,57 @@ int main(int argc, char** argv) {
     /* Get the scene from the helper. */
     Scene* p_scene = helper.GetScene();
 
-    /* Create our light. */
-    shdr::PointLight light(Vector3DF(-10, 20, 0), Vector3DF(1,1,1));
+    static constexpr auto lights = std::to_array<shdr::PointLight>(
+        {
+            { Vector3DF(-10, 20, 0), Vector3DF(1,1,1) }
+        }
+    );
 
-    /* Create color shader. */
-    auto color_shader = shdr::FlatColor::Create(Vector3DF(0.75, 0.75, 0.75));
+    /* Create gray ground shader. */
+    auto gray_shader = shdr::Lambertian::Create(
+        shdr::FlatColor::Create(Vector3DF(0.75, 0.75, 0.75)),
+        lights
+    );
 
-    //auto shader = eng::BlinnPhongShader::Create(eng::ray{ eng::Vector3DF{ 0,0,0 }, eng::Vector3DF(3, 4,5)}, 20.0);
-    //auto shader = eng::shdr::Lambertian::Create(eng::Ray{ eng::Vector3DF{ 0,0,0 }, eng::Vector3DF(3, 4,5)});
-    /*
-    p_scene->EmplaceShape<eng::shape::Triangle>(eng::Vector3DF{-4.426795, 1.13923, -7}, eng::Vector3DF{-4.833013, -0.44282, -5}, eng::Vector3DF{-4.45, -0.779423, -5});
-    p_scene->EmplaceShape<shape::Sphere>(Vector3DF(-5, -5, -10), 2)
-        .BindShader(shdr::Mirror::Create());
-    */
+    /* Create purple ball shader. */
+    auto ball_shader = shdr::Lambertian::Create(
+        shdr::FlatColor::Create(Vector3DF(1, 0, 1)), lights
+    );
+
+    /* Create color shaders. */
+    auto red_shader = shdr::Lambertian::Create(
+        shdr::FlatColor::Create(Vector3DF(1, 0, 0)), lights
+    );
+    auto green_shader = shdr::Lambertian::Create(
+        shdr::FlatColor::Create(Vector3DF(0, 1, 0)), lights
+    );
+    auto blue_shader = shdr::Lambertian::Create(
+        shdr::FlatColor::Create(Vector3DF(0, 0, 1)), lights
+    );
+
+    static constexpr float TriBaseX = -7.0;
+    static constexpr float TriBaseY = 22.0;
+    static constexpr float TriBaseZ = -30.0;
+
+    /* Create The Triforce. */
+    p_scene->EmplaceShape<shape::Triangle>(
+        Vector3DF(TriBaseX-10, TriBaseY, TriBaseZ), Vector3DF(TriBaseX, TriBaseY, TriBaseZ), Vector3DF(TriBaseX-5, TriBaseY+10, TriBaseZ)
+    ).BindShader(blue_shader);
+    p_scene->EmplaceShape<shape::Triangle>(
+        Vector3DF(TriBaseX, TriBaseY, TriBaseZ), Vector3DF(TriBaseX+10, TriBaseY, TriBaseZ), Vector3DF(TriBaseX+5, TriBaseY+10, TriBaseZ)
+    ).BindShader(green_shader);
+    p_scene->EmplaceShape<shape::Triangle>(
+        Vector3DF(TriBaseX-5, TriBaseY+10, TriBaseZ), Vector3DF(TriBaseX+5, TriBaseY+10, TriBaseZ), Vector3DF(TriBaseX, TriBaseY+20, TriBaseZ)
+    ).BindShader(red_shader);
+
     /* Create ground plane. */
     p_scene->EmplaceShape<shape::Triangle>(
         Vector3DF(-200, 0, 50), Vector3DF(200, 0, 50), Vector3DF(0, 0, -2000)
-    ).BindShader(shdr::Lambertian::Create(color_shader, std::array{
-        light
-    }));
+    ).BindShader(gray_shader);
 
     /* Make a sphere. */
-    p_scene->EmplaceShape<eng::shape::Sphere>(eng::Vector3DF{3, 6, -10}, 5)
-        .BindShader(shdr::Lambertian::Create(
-            color_shader,
-            std::array {
-                light
-            }
-    ));
+    p_scene->EmplaceShape<eng::shape::Sphere>(eng::Vector3DF{4, 6, -10}, 5)
+        .BindShader(ball_shader);
 
     /* And another one. */
     p_scene->EmplaceShape<eng::shape::Sphere>(eng::Vector3DF{-9, 6, -14}, 5)
