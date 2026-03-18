5024aac54457db38b371cea0bc8a8e47a4c12949
Zachary Kirtz <zach.kirtz@gmail.com>
2026-03-18 13:17:08 -0500

    render_lib: cameras: Add a GetImageDimensions function and make cameras inherit from ObjectBase.

--- render_lib/include/engine/eng_ICamera.h
+++ render_lib/include/engine/eng_ICamera.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <engine/eng_Ray.h>
+#include <utility>
 
 namespace eng {
 
@@ -9,8 +10,10 @@ public:
     virtual ~ICamera() = default;
 
     virtual Ray GenerateRay(float i, float j) = 0;
+    virtual constexpr std::pair<float, float> GetImageDimensions() = 0;
 
     constexpr virtual float GetMinT() const = 0;
+
 private:
     /* ... */
 }; // class ICamera
