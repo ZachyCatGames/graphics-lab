f5283d6e924ca5b57df80bec011b8795463651f7
Zachary Kirtz <zach.kirtz@gmail.com>
2026-04-12 02:26:28 -0500

    render_lib: Add attribution for string hash func.

--- render_lib/include/engine/eng_Hash.h
+++ render_lib/include/engine/eng_Hash.h
@@ -10,6 +10,14 @@ struct Hash : std::hash<T> {};
 
 template<>
 struct Hash<std::string_view> {
+    /**
+     * djb2 hash algorithm, found via:
+     * http://www.cse.yorku.ca/~oz/hash.html
+     * https://stackoverflow.com/questions/7968674/unexpected-collision-with-stdhash
+     * 
+     * @param str  String to hash
+     * @return Hash
+     */
     constexpr size_t operator()(const std::string_view& str) const {
         size_t hash = 5381;
         for(auto c : str) {
