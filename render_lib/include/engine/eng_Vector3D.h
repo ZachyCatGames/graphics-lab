96bed5bb623d2b8d2170c2c25bd778fc369f5c6b
Zachary Kirtz <zach.kirtz@gmail.com>
2026-04-19 01:30:42 -0500

    Vector: Use the pipe operator to apply the transform in the negation operator... now I go

--- render_lib/include/engine/eng_Vector3D.h
+++ render_lib/include/engine/eng_Vector3D.h
@@ -69,7 +69,7 @@ public:
     [[nodiscard]] constexpr ValueType get_ptr() noexcept { return &e[0]; }
 
     [[nodiscard]] constexpr Vector<ValueT, N> operator-() const noexcept {
-        return Vector(std::ranges::views::transform(e, std::negate()));
+        return Vector(e | std::ranges::views::transform(std::negate()));
     }
 
     template<typename Self>
