56208b7a29595fbc65c8b1ca95e1f9f959182c08
Zachary Kirtz <zach.kirtz@gmail.com>
2026-05-06 11:51:24 -0500

    Engine: Add helpers that call Create* functions on IObjectFactory and implement loading textures from png files.

--- render_lib/include/engine/eng_Engine.h
+++ render_lib/include/engine/eng_Engine.h
@@ -1,9 +1,13 @@
 #pragma once
+#include "engine/eng_RenderBuffer.h"
+#include <engine/eng_Handle.h>
 #include <engine/eng_HandleGraphicsArgs.h>
 #include <engine/eng_IObjectFactory.h>
 #include <engine/eng_IRenderer.h>
 #include <engine/eng_ObjectBase.h>
 #include <engine/eng_Scene.h>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 #include <memory>
 
 namespace eng {
@@ -13,19 +17,63 @@ public:
     int Initialize(const GraphicsArgs& args);
     int Initialize(int argc, char** argv);
 
-    IObjectFactory* GetObjectFactory() const { return m_pObjFactory.get(); }
+    [[nodiscard]] IObjectFactory* GetObjectFactory() const { return m_pObjFactory.get(); }
+
+    [[nodiscard]] std::string GetRenderString() const { return m_renderMode; }
+
+    /** Window APIs. */
+    [[nodiscard]] GLFWwindow* GetGlfwWindow() const { return m_pWindow; }
+    [[nodiscard]] bool ShouldClose() const { return glfwWindowShouldClose(m_pWindow); }
+
 
     /* NOTE: This is for debugging!!! */
-    IRenderer* GetRenderer() const { return m_pRenderer.get(); }
+    [[nodiscard]] IRenderer* GetRenderer() const { return m_pRenderer.get(); }
 
-    const auto& GetActiveScene() const { return m_pActiveScene; }
-    void UpdateActiveScene(const Handle<Scene>& scene);
+    [[nodiscard]] const auto& GetActiveScene() const { return m_pActiveScene; }
 
-    void RenderActiveScene(std::string_view cameraName, Handle<RenderBuffer> fb) {
+    void RenderActiveScene(std::string_view cameraName, const Handle<RenderBuffer>& fb) {
         m_pRenderer->PreRender();
 
         m_pRenderer->Render(cameraName, fb);
     }
+
+    const Handle<RenderBuffer>& GetDisplayRenderBuffer() {
+        return m_displayRenderBuffer;
+    }
+
+    Handle<Texture> OpenTextureFromPNG(std::string_view path);
+
+    /* Object creation wrappers. */
+    Handle<eng::ICamera> CreatePerspectiveCamera(const Vector3DF& pos, const Vector3DF& dir, float focal_length, int img_width, int img_height, float img_plane_height) {
+        return m_pObjFactory->CreatePerspectiveCamera(pos, dir, focal_length, img_width, img_height, img_plane_height);
+    }
+
+    Handle<IShape> CreateSphere(const Vector3DF& pos, float radius, const Handle<IShader>& shader) {
+        return m_pObjFactory->CreateSphere(pos, radius, shader);
+    }
+    Handle<IShape> CreateTriangle(const Vector3DF& a, const Vector3DF& b, const Vector3DF& c, const Handle<IShader>& shader) {
+        return m_pObjFactory->CreateTriangle(a, b, c, shader);
+    }
+    Handle<IShape> CreateMesh(const std::vector<Vertex>& vertices, const Vector3DF& position, const Handle<IShader>& shader) {
+        return m_pObjFactory->CreateMesh(vertices, position, shader);
+    }
+
+    Handle<IShader> CreateLambertian(const Material& material, const std::vector<shdr::PointLight>& lights) {
+        return m_pObjFactory->CreateLambertian(material, lights);
+    }
+    Handle<IShader> CreatePhong(const Material& material, const std::vector<shdr::PointLight>& lights) {
+        return m_pObjFactory->CreatePhong(material, lights);
+    }
+
+    Handle<Texture> CreateTexture(const float* textureData, size_t width, size_t height) {
+        return m_pObjFactory->CreateTexture(textureData, width, height);
+    }
+
+    Handle<ExportableRenderBuffer> CreateExportableRenderBuffer(size_t width, size_t height) {
+        return m_pObjFactory->CreateExportableRenderBuffer(width, height);
+    }
+private:
+    void InitializeOpenGL();
 private:
     GraphicsArgs m_gArgs;
 
@@ -33,6 +81,9 @@ private:
 
     std::unique_ptr<IObjectFactory> m_pObjFactory;
     std::unique_ptr<IRenderer> m_pRenderer;
+
+    GLFWwindow* m_pWindow;
+    Handle<RenderBuffer> m_displayRenderBuffer;
 
     std::string m_renderMode;
 }; // class Engine
--- render_lib/sources/eng_Engine.cpp
+++ render_lib/sources/eng_Engine.cpp
@@ -1,3 +1,4 @@
+#include "engine/gl/eng_IRenderable.h"
 #include <engine/eng_Engine.h>
 
 #include <engine/raytracer/eng_ObjectFactory.h>
@@ -6,7 +7,55 @@
 #include <engine/gl/eng_ObjectFactory.h>
 #include <engine/gl/eng_Renderer.h>
 
+#include <engine/png++/png.hpp>
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <engine/gl/eng_IRenderable.h>
+
+#include <cstdlib>
+#include <print>
+
 namespace eng {
+
+namespace {
+
+void InitializeGlfw() {
+    if (!glfwInit())
+        exit(-1);
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+}
+
+} // namespace
+
+void Engine::InitializeOpenGL() {
+    /* Initialize glew. */
+    glewExperimental = GL_TRUE;
+    GLenum err = glewInit();
+    if (err != GLEW_OK) {
+        std::print("GLEW Error: {}\n", err);
+        exit(-1);
+    }
+
+    std::printf("Renderer: %s\n", glGetString (GL_RENDERER));
+    std::printf("OpenGL version supported: %s\n", glGetString(GL_VERSION));
+
+    /* Enable depth test. */
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LESS);
+    //glClearColor(0.0, 0.7, 1.0, 1.0);
+    glClearColor(7.0 / 255.0, 35.0 / 255.0, 220.0 / 255.0, 1.0);
+
+    /* Setup viewport dims. */
+    glViewport(0, 0, m_displayRenderBuffer->GetWidth(), m_displayRenderBuffer->GetHeight());
+
+
+}
 
 int Engine::Initialize(int argc, char** argv) {
     /* Initialize Graphics Args. */
@@ -29,9 +78,54 @@ int Engine::Initialize(int argc, char** argv) {
 
         /* Setup renderer. */
         m_pRenderer = std::make_unique<gl::Renderer>(m_pActiveScene);
+    
+        /* Init glfw. */
+        InitializeGlfw();
+
+        /* Create a GLFW window. */
+        m_pWindow = glfwCreateWindow(m_gArgs.windowWidth, m_gArgs.windowHeight, "test", nullptr, nullptr);
+
+        /* Disable the mouse cursor and enable raw input if supported. */
+        glfwSetInputMode(m_pWindow, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+        if (glfwRawMouseMotionSupported())
+            glfwSetInputMode(m_pWindow, GLFW_RAW_MOUSE_MOTION, GL_TRUE);
+
+        /* Setup main window. */
+        m_displayRenderBuffer = m_pObjFactory->CreateDisplayRenderBuffer(m_pWindow);
+        assert(m_displayRenderBuffer);
+
+        /* Bind the main window as the current context. */
+        gl::SetContextCurrent(m_displayRenderBuffer);
+
+        /* Initialize OpenGL. */
+        this->InitializeOpenGL();
     }
 
     return 0;
+}
+
+Handle<Texture> Engine::OpenTextureFromPNG(std::string_view path) {
+    png::image<png::rgb_pixel> texPngImage;
+    texPngImage.read(path.data());
+
+    int pngWidth = texPngImage.get_width();
+    int pngHeight = texPngImage.get_height();
+    std::print("PNG Width:  {}\n", pngWidth);
+    std::print("PNG Height: {}\n", pngHeight);
+
+    std::vector<float> texData(pngWidth * pngHeight * 3);
+
+    size_t idx = 0;
+    for (int row = 0; row < pngHeight; row++) {
+        for (int col = 0; col < pngWidth; col++) {
+            png::rgb_pixel pixel = texPngImage[pngHeight - row - 1][col];
+            texData[idx++] = ((float)pixel.red) / 255.0f;
+            texData[idx++] = ((float)pixel.green) / 255.0f;
+            texData[idx++] = ((float)pixel.blue) / 255.0f;
+        }
+    }
+
+    return this->CreateTexture(texData.data(), pngWidth, pngHeight);
 }
 
 } // namespace eng
